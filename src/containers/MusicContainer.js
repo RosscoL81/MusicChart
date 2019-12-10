a4d300cb782f3be73efd54471a8362157fe07ebf
Ross Lorimer <wross.lorimer@googlemail.com>
2019-12-10 22:10:31 +0000

    name and artist displayed

---
+++ src/containers/MusicContainer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import SongSelector from '../components/SongSelector'
+import SongDetail from '../components/SongDetail'
+
+
+class MusicContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      songs: [],
+      currentSong: null
+    };
+    this.handleSongSelected = this.handleSongSelected.bind(this);
+  }
+
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+    fetch(url)
+    .then(res => res.json())
+    .then(songs => this.setState({songs: songs.feed.entry}))
+    .catch(err => console.error);
+  }
+
+  handleSongSelected(index) {
+    const selectedSong = this.state.songs[index];
+    this.setState({currentSong: selectedSong})
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Music Charts</h1>
+        <SongSelector
+        songs={this.state.songs}
+        onSongSelected={this.handleSongSelected}
+        />
+        <SongDetail song={this.state.currentSong}/>
+      </div>
+    );
+  }
+
+}
+export default MusicContainer;
